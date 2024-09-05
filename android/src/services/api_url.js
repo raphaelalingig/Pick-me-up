661357d03953df3867d28bc5056495b57430c312
Jundy25 <127575259+Jundy25@users.noreply.github.com>
2024-09-05 21:07:16 +0800

    f

--- android/src/services/api_url.js
+++ android/src/services/api_url.js
@@ -1,5 +1,6 @@
-const API_URL = "http://172.22.185.171:8000/api/user/";
-// const API_URL = "http://192.168.1.13:8000/api/user/";
+// const API_URL = "http://192.168.72.124:8000/api/user/";
+// const API_URL = "http://192.168.1.18:8000/api/user/";
+const API_URL = "http:/192.168.1.6:8000/api/user/";
 
 
 export default API_URL;
