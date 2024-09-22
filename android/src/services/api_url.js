c5e370c3d6e153795be9479e92f9bb0e09f99bc3
Buwanding <buwandingaladdin@gmail.com>
2024-09-22 22:07:22 +0800

    myurl

--- android/src/services/api_url.js
+++ android/src/services/api_url.js
@@ -1,7 +1,7 @@
 // const API_URL = "http://192.168.72.124:8000/api/user/";
 // const API_URL = "http://192.168.1.18:8000/api/user/";
-// const API_URL = "http:/192.168.1.6:8000/api/user/";
-const API_URL = "http:/192.168.1.26:8000/api/user/";
+const API_URL = "http:/192.168.1.6:8000/api/user/";
+// const API_URL = "http:/192.168.1.26:8000/api/user/";
 
 
 
