16c92794510d7e8233fa4011b841be5887dcb2bc
Jundy25 <127575259+Jundy25@users.noreply.github.com>
2024-09-26 01:51:36 +0800

    Update TrackingRider.js

--- android/src/customer/pages/TrackingRider.js
+++ android/src/customer/pages/TrackingRider.js
@@ -1,62 +1,145 @@
-import React from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import {
   View,
   Text,
   StyleSheet,
   TouchableOpacity,
-  Dimensions,
+  ImageBackground,
+  Alert,
+  ScrollView,
+  RefreshControl,
+  Dimensions
 } from "react-native";
+import { Button } from "react-native-paper";
+import userService from '../../services/auth&services';
 import { MaterialCommunityIcons, FontAwesome5 } from '@expo/vector-icons'; // For icons
 
 const { height, width } = Dimensions.get('window'); // Get screen dimensions
 
-const DeliveryConfirmationScreen = ({ navigation }) => {
-  return (
-    <View style={styles.container}>
-      {/* Header Section covering 20% */}
-      <View style={styles.header}>
-        <FontAwesome5 name="users" size={40} color="black" />
-        <Text style={styles.serviceTitle}>Pakyaw</Text>
-      </View>
+const DeliveryConfirmationScreen = ({navigation}) => {
+  const [bookDetails, setBookDetails] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [refreshing, setRefreshing] = useState(false);
 
-      {/* Content Section covering 80% */}
-      <View style={styles.content}>
-        {/* Booking Confirmation Message */}
-        <View style={styles.messageContainer}>
-          <View style={styles.successBox}>
-            <MaterialCommunityIcons name="motorbike" size={80} color="black" />
-            <View style={styles.successTextContainer}>
-              <Text style={styles.successMessage}>Ride Successfully Booked</Text>
-              <Text style={styles.statusMessage}>Rider is on the way...</Text>
+
+  const fetchLatestAvailableRide = async () => {
+    try {
+      const ride = await userService.checkActiveBook();
+      setBookDetails(ride.rideDetails);
+    } catch (error) {
+      Alert.alert("Error", "Failed to retrieve the latest available ride.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchLatestAvailableRide();
+  }, []);
+
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    // Navigate to Home screen
+    navigation.navigate("Home");
+    setRefreshing(false);
+  }, [navigation]);
+
+  const handleCancel = async () => {
+    
+    console.log("Attempting to cancel ride");
+    
+    setIsLoading(true);
+    try {
+      const response = await userService.cancel_ride(bookDetails.ride_id);
+      console.log("Cancel ride response:", response.data);
+  
+      if (response.data && response.data.message) {
+        Alert.alert("Success", response.data.message);
+        navigation.navigate("Home"); // Redirect to home screen or wherever appropriate
+      } else {
+        Alert.alert("Error", "Failed to cancel the ride. Please try again.");
+      }
+    } catch (error) {
+      console.error("Failed to Cancel Ride", error.response ? error.response.data : error.message);
+      if (error.response && error.response.status === 400) {
+        Alert.alert("Error", error.response.data.error || "This ride is no longer available.");
+        navigation.goBack(); // Go back to the previous screen
+      } else {
+        Alert.alert("Error", "An error occurred. Please try again.");
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  
+
+  if (isLoading || !bookDetails) {
+    return (
+      <View style={styles.loadingContainer}>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+  
+  return (
+    <ScrollView
+    contentContainerStyle={styles.scrollViewContent}
+    refreshControl={
+      <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+    }
+    >
+      <View style={styles.container}>
+        {/* Header Section covering 20% */}
+        <View style={styles.header}>
+          <FontAwesome5 name="users" size={40} color="black" />
+          <Text style={styles.serviceTitle}>{bookDetails.ride_type}</Text>
+        </View>
+
+        {/* Content Section covering 80% */}
+        <View style={styles.content}>
+          {/* Booking Confirmation Message */}
+          <View style={styles.messageContainer}>
+            <View style={styles.successBox}>
+              <MaterialCommunityIcons name="motorbike" size={80} color="black" />
+              <View style={styles.successTextContainer}>
+                <Text style={styles.successMessage}>Matched Successfully</Text>
+                <Text style={styles.statusMessage}>Rider is on the way...</Text>
+              </View>
             </View>
           </View>
-        </View>
 
-        {/* Rider Details Section */}
-        <View style={styles.detailsContainer}>
-          <Text style={styles.subTitle}>Rider Details</Text>
-          <View style={styles.detailRow}>
-            <MaterialCommunityIcons name="account" size={24} color="black" />
-            <Text style={styles.detailText}>Martzel Amalberrt Arroyo</Text>
+          {/* Rider Details Section */}
+          <View style={styles.detailsContainer}>
+            <Text style={styles.subTitle}>Rider Details</Text>
+            <View style={styles.detailRow}>
+              <MaterialCommunityIcons name="account" size={24} color="black" />
+              <Text style={styles.detailText}>{bookDetails.rider ? `${bookDetails.rider.first_name} ${bookDetails.rider.last_name}` : 'N/A'}</Text>
+            </View>
+            <View style={styles.detailRow}>
+              <MaterialCommunityIcons name="phone" size={24} color="black" />
+              <Text style={styles.detailText}>{bookDetails.rider ? `${bookDetails.rider.mobile_number}` : 'N/A'}</Text>
+            </View>
+            <View style={styles.detailRow}>
+              <MaterialCommunityIcons name="motorbike" size={24} color="black" />
+              <Text style={styles.detailText}>Motor: Yamaha Sniper 150</Text>
+            </View>
           </View>
-          <View style={styles.detailRow}>
-            <MaterialCommunityIcons name="phone" size={24} color="black" />
-            <Text style={styles.detailText}>0978-345-1234</Text>
-          </View>
-          <View style={styles.detailRow}>
-            <MaterialCommunityIcons name="motorbike" size={24} color="black" />
-            <Text style={styles.detailText}>Motor: Yamaha Sniper 150</Text>
-          </View>
-        </View>
 
-        {/* Go Back Button */}
-        <TouchableOpacity onPress={() => navigation.goBack()}>
-          <View style={styles.goBackButton}>
-            <Text style={styles.goBackButtonText}>Go Back</Text>
-          </View>
-        </TouchableOpacity>
+          {/* Go Back Button */}
+          <TouchableOpacity>
+            <View style={styles.goBackButton}>
+              <Text style={styles.goBackButtonText}>Contact Rider</Text>
+            </View>
+          </TouchableOpacity>
+          
+          <TouchableOpacity onPress={handleCancel}>
+            <View style={styles.goBackButton}>
+              <Text style={styles.goBackButtonText}>Cancel</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
