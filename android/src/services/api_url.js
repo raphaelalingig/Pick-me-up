1e7fa38722588076c6fe681f0ed07075cef9c6b4
Jundy25 <127575259+Jundy25@users.noreply.github.com>
2024-11-10 11:09:55 +0800

    delpoyed backend

--- android/src/services/api_url.js
+++ android/src/services/api_url.js
@@ -1,6 +1,6 @@
 // const API_URL = "http://192.168.72.124:8000/api/user/";
 // const API_URL = "http://192.168.1.18:8000/api/user/";
-const API_URL = "http:/192.168.58.124:8000/api/user/";
-// const API_URL = "http://192.168.193.124:8000/api/user/";
+const API_URL = "https://backendpickmeuptrial-production.up.railway.app/api/user/";
+// const API_URL = "http://192.168.58.124:8000/api/user/";
 
 export default API_URL;
