35e607843dd79baf3157a4c62c2dc8753a0ad3d7
Jundy25 <127575259+Jundy25@users.noreply.github.com>
2024-12-07 20:02:44 +0800

    api

--- src/services/api_url.js
+++ src/services/api_url.js
@@ -1,5 +1,4 @@
-// const API_URL =
-//   "https://backendpickmeuptrial-production.up.railway.app/api/user/";
-const API_URL = "http://192.168.1.16:8000/api/user/";
+const API_URL =
+  "https://backendpickmeuptrial-production.up.railway.app/api/user/";
 
 export default API_URL;
